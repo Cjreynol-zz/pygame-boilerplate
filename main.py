9ffb73fd63e0a98357e1a5e86ac9302039360892
Chad Reynolds <cjreynol13@aol.com>
2018-08-01 23:08:45 -0500

    Add simple animation and improve rendering
    Incorporating some advice from the PyGame website, instead of
    drawing the entire screen every frame I:
    - have a separate background Surface
    - cover the sprite with the appropriate portion of background
    - move the sprite on input (as before)
    - draw the sprite in its position with the proper frame of animation
    - update the portions of the screen associated with the cover and draw

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from os             import path
-from pygame         import draw, display, event, image, init, quit
+from pygame         import draw, display, event, image, init, quit, Surface
 
-from pygame.locals  import QUIT, KEYDOWN, KEYUP
-from pygame.locals  import K_DOWN, K_ESCAPE, K_LEFT, K_UP, K_RIGHT
+from pygame.locals  import (QUIT, KEYDOWN, KEYUP,
+                            K_DOWN, K_ESCAPE, K_LEFT, K_UP, K_RIGHT)
 
 from pygame.time    import Clock
 
 
 # globals
 FPS = 30
-SCREEN_SIZE = 1024
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BACKGROUND_COLOR = (255, 255, 255)
+TITLE = "LD42 Testing"
+IMG_DIR = "img"
 
 done = False
 clock = Clock()
-screen = display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
-display.set_caption("LD42 Testing")
+screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+display.set_caption(TITLE)
 
-ghost_sprite = image.load(path.join("img", "pix_boo.png"))
+background = Surface(screen.get_size()).convert()
+background.fill(BACKGROUND_COLOR)
+screen.blit(background, (0, 0))
+display.flip()
+
+ghost_sprites = [image.load(path.join(IMG_DIR, "pix_boo.png"))
+                    .convert_alpha(),
+                    image.load(path.join(IMG_DIR, "pix_boo2.png"))
+                    .convert_alpha()]
+animation_index = 0
+animation_speed = FPS // 2
+animation_index_limit = len(ghost_sprites) * animation_speed
+
 ghost_x, ghost_y = 10, 10
-ghost_vel = 10
+ghost_size = ghost_sprites[0].get_width(), ghost_sprites[0].get_height()
+ghost_vel = 32
+last_rect = None
 
 
 # helpers
@@ -40,15 +58,34 @@ def handle_events(event_list):
         elif event.type == KEYDOWN and event.key == K_LEFT:
             ghost_x -= ghost_vel
 
+def clear():
+    global last_rect
+
+    ghost_pos = ghost_x, ghost_y
+    last_rect = screen.blit(background, ghost_pos, ghost_pos + ghost_size)
+    
 
 def render():
-    screen.fill((255, 255, 255))
-    screen.blit(ghost_sprite, (ghost_x, ghost_y))
-    display.flip()
+    global animation_index
+
+    animation_index += 1
+    if animation_index >= animation_index_limit:
+        animation_index = 0
+
+    sprite_rect = screen.blit(ghost_sprites[animation_index 
+                                                // animation_speed], 
+                                (ghost_x, ghost_y))
+
+    dirty_rects = [sprite_rect]
+    if last_rect is not None:
+        dirty_rects.append(last_rect)
+
+    display.update(dirty_rects)
 
 def main():
     while not done:
         clock.tick(FPS)
+        clear()
         handle_events(event.get())
         render()
 
